80deebc014a355db526efb2945cf36ca8bb5eec6
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-07-06 13:50:45 -0400

    Remove unused helper function and test data.

--- vc-generator/helpers.js
+++ vc-generator/helpers.js
@@ -2,7 +2,6 @@
  * Copyright 2023 Digital Bazaar, Inc. All Rights Reserved
  */
 import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
-import crypto from 'crypto';
 import {decodeSecretKeySeed} from 'bnid';
 
 export const getMultikey = async ({seedMultibase}) => {
@@ -20,29 +19,6 @@ export const getMultikey = async ({seedMultibase}) => {
   signer.id = `${issuer}#${key.publicKeyMultibase}`;
   return {signer, issuer};
 };
-
-/**
- * Creates a hash digest function using node's crypto lib.
- *
- * @param {object} options - Options to use.
- * @param {string} [options.algorithm = 'sha256'] - An openssl
- *   compatible hashing algorithm.
- *
- * @returns {Function} The hashing algorithm.
- */
-export function hashDigest({algorithm = 'sha256'} = {}) {
-  /**
-   * Hashes a string of data using SHA-256.
-   *
-   * @param {object} options - Options to use.
-   * @param {string} options.string - The string to hash.
-   *
-   * @returns {Promise<Uint8Array>} The hash digest.
-   */
-
-  return async ({string}) => new Uint8Array(
-    crypto.createHash(algorithm).update(string).digest());
-}
 
 export function invalidCreateProof({
   addCreated = true,
