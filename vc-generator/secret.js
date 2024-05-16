e3997b6744e172273b9e8398cf39caf60cbb92e2
Andrew Jones <ajones@digitalbazaar.com>
2024-05-16 14:38:22 +0000

    Use suiteName over suite in vc-gen files.

--- vc-generator/secret.js
+++ vc-generator/secret.js
@@ -11,22 +11,23 @@ const require = createRequire(import.meta.url);
 export const TEST_KEY_SEED =
   'z1AZVaiqEq3kXaf4DJD5qXUfdJBFbW1JNe4FF58HwMgVE6u';
 
-export function getSerializedKeyPair({suite, keyType}) {
-  const {serializedKeys} = cryptosuites.get(suite);
+export async function getKeyPair({suiteName, keyType}) {
+  const {serializedKeys, multikey} = cryptosuites.get(suiteName);
   const keyDir = './keys';
   if(!serializedKeys) {
     throw new Error(`Unrecognized suite: ${suite}`);
   }
   if(typeof serializedKeys === 'string') {
-    return require(`${keyDir}/${serializedKeys}`);
+    return multikey.from(require(`${keyDir}/${serializedKeys}`));
   }
   const keyPath = serializedKeys[keyType];
   if(!keyPath) {
     throw new Error(`Unrecognized keyType ${keyType} for suite ${suite}`);
   }
-  return require(`${keyDir}/${keyPath}`);
+  return multikey.from(require(`${keyDir}/${keyPath}`));
 }
 
-export async function getMultiKey({suite, keyType}) {
-  const keyDoc = getSerializedKeyPair({suite, keyType});
+export async function getMultiKey({suiteName, keyType}) {
+  const keyPair = await getKeyPair({suiteName, keyType});
+  return {issuer: keyPair.controller, signer: keyPair.signer()};
 }
