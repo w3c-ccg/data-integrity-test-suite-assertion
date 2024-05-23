d90a927615719e8dabccee270c77344e3b78f68e
Andrew Jones <ajones@digitalbazaar.com>
2024-05-23 20:34:57 +0000

    Begin removing cryptosuite specific logic from vc-gen.

--- vc-generator/index.js
+++ vc-generator/index.js
@@ -20,35 +20,37 @@ const vcCache = new Map();
  *
  * @param {object} options - Options to use.
  * @param {string} options.suiteName - A suite name.
- * @param {string} options.keyType - A keyType.
+ * @param {object} options.key - A key.
+ * @param {object} options.cryptosuite - A cryptosuite for the test.
  * @param {Array<string>} options.mandatoryPointers - An array of JSON pointers.
  * @param {Array<string>} options.selectivePointers - An array of JSON pointers.
  * @param {boolean} options.verify - If a verify suite is needed.
  * @param {object} options.optionalTests - Options for running optional tests.
+ * @param {object} [options.testVector = validVc] - A credential to use for
+ * test data.
  *
  * @returns {Promise<Map>} Returns a Map of test data.
  */
 export async function generateTestData({
   suiteName = 'eddsa-2022',
-  keyType,
+  key,
+  cryptosuite,
   mandatoryPointers,
   selectivePointers,
   verify,
-  optionalTests
+  optionalTests,
+  testVector = validVc
 } = {}) {
   if(!vcCache.get(suiteName)) {
     vcCache.set(suiteName, _initCache());
   }
-  const {signer, issuer} = await getMultiKey({
-    keyType,
-    suiteName
-  });
-  const credential = klona(validVc);
-  credential.issuer = issuer;
+  const credential = klona(testVector);
+  credential.issuer = key.controller;
+  const signer = key.signer();
   const vcGenerators = getGenerators(optionalTests);
   for(const [id, generator] of vcGenerators) {
     const {suite, selectiveSuite} = getSuites({
-      suiteName,
+      cryptosuite,
       signer,
       mandatoryPointers,
       selectivePointers,
