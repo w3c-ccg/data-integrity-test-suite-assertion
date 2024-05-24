95a33ad56d13db6f244c8dc22eba21d769c4ddb2
Andrew Jones <ajones@digitalbazaar.com>
2024-05-24 14:18:01 +0000

    Further cutdown on unneeded asyncs.

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ export function checkDataIntegrityProofVerifyErrors({
   }); // end describe
 }
 
-async function runDataIntegrityProofVerifyTests({
+function runDataIntegrityProofVerifyTests({
   endpoints,
   expectedProofType,
   testDescription,
