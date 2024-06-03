45d8e40dc0f0312b4835ec6edaa0b4d2699c15b9
Andrew Jones <ajones@digitalbazaar.com>
2024-06-03 13:56:51 +0000

    Disable JsonLdDocumentLoader to prevent docs being tagged static.

--- vc-generator/documentLoader.js
+++ vc-generator/documentLoader.js
@@ -2,6 +2,8 @@
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
 import {contextMap} from './contexts.js';
+// disable JsonLdDocumentLoader support for now
+/*
 import {JsonLdDocumentLoader} from 'jsonld-document-loader';
 const jdl = new JsonLdDocumentLoader();
 
@@ -9,5 +11,13 @@ const jdl = new JsonLdDocumentLoader();
 for(const [key, value] of contextMap) {
   jdl.addStatic(key, value);
 }
+*/
 
-export const documentLoader = jdl.build();
+export const documentLoader = async url => {
+  const document = contextMap.get(url);
+  return {
+    contextUrl: null,
+    documentUrl: url,
+    document
+  };
+};
