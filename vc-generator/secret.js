9be820958dc389e4e1d2d29a6d7c9b8224792bbf
Andrew Jones <ajones@digitalbazaar.com>
2024-06-04 17:54:35 +0000

    Default key uses from & serialized keyPair.

--- vc-generator/secret.js
+++ vc-generator/secret.js
@@ -7,6 +7,14 @@ import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
 // proof tests.
 export const TEST_KEY_SEED =
   'z1AZVaiqEq3kXaf4DJD5qXUfdJBFbW1JNe4FF58HwMgVE6u';
+const TEST_KEY_PAIR = {
+  "id": "did:key:z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT#z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT",
+  "@context": "https://w3id.org/security/multikey/v1",
+  "type": "Multikey",
+  "controller": "did:key:z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT",
+  "publicKeyMultibase": "z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT",
+  "secretKeyMultibase": "zrv3rbPamVDGvrm7LkYPLWYJ35P9audujKKsWn3x29EUiGwwhdZQd1iHhrsmZidtVALBQmhX3j9E5Fvx6Kr29DPt6LH"
+};
 
 export const getDefaultKey = async () =>
-  Ed25519Multikey.generate({seed: TEST_KEY_SEED});
+  Ed25519Multikey.from(TEST_KEY_PAIR);
