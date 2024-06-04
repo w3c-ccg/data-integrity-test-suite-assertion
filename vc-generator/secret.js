015986a7cf74fd05c9c22499bf97b6f5b7a6557b
Andrew Jones <ajones@digitalbazaar.com>
2024-06-04 18:03:16 +0000

    Lint secret & keys.index.js.

--- vc-generator/secret.js
+++ vc-generator/secret.js
@@ -8,12 +8,14 @@ import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
 export const TEST_KEY_SEED =
   'z1AZVaiqEq3kXaf4DJD5qXUfdJBFbW1JNe4FF58HwMgVE6u';
 const TEST_KEY_PAIR = {
-  "id": "did:key:z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT#z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT",
-  "@context": "https://w3id.org/security/multikey/v1",
-  "type": "Multikey",
-  "controller": "did:key:z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT",
-  "publicKeyMultibase": "z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT",
-  "secretKeyMultibase": "zrv3rbPamVDGvrm7LkYPLWYJ35P9audujKKsWn3x29EUiGwwhdZQd1iHhrsmZidtVALBQmhX3j9E5Fvx6Kr29DPt6LH"
+  id: 'did:key:z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT#z6MkwXG2Wje' +
+    'QnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT',
+  '@context': 'https://w3id.org/security/multikey/v1',
+  type: 'Multikey',
+  controller: 'did:key:z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT',
+  publicKeyMultibase: 'z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT',
+  secretKeyMultibase: 'zrv3rbPamVDGvrm7LkYPLWYJ35P9audujKKsWn3x29EUiGwwhdZ' +
+    'Qd1iHhrsmZidtVALBQmhX3j9E5Fvx6Kr29DPt6LH'
 };
 
 export const getDefaultKey = async () =>
