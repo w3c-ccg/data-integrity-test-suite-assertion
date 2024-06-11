e7745c7ca13001023c803a487327f8962edbf418
Andrew Jones <ajones@digitalbazaar.com>
2024-06-11 11:25:00 -0400

    Update vc-generator/secret.js remove FIXME from secret.js
    Co-authored-by: BigBlueHat <byoung@bigbluehat.com>

--- vc-generator/secret.js
+++ vc-generator/secret.js
@@ -4,8 +4,6 @@
 import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
 import {createRequire} from 'node:module';
 
-//FIXME remove this once node has non-experimental support
-//for es6 import and json
 const require = createRequire(import.meta.url);
 const defaultKey = require('./defaultKey.json');
 // This is only for use inside this test suite for generating vcs for the verify
