c1ff7adc62cfd2da944cfdf468d69a9517475219
Andrew Jones <ajones@digitalbazaar.com>
2024-06-17 17:06:20 +0000

    In beforeEach add test.cell for verify suite.

--- suites/verify.js
+++ suites/verify.js
@@ -12,33 +12,35 @@ export function runDataIntegrityProofVerifyTests({
   testDataOptions,
   optionalTests
 }) {
-  const columnId = testDescription;
   return describe(testDescription, function() {
     const [verifier] = endpoints;
     if(!verifier) {
       throw new Error(`Expected ${vendorName} to have a verifier.`);
     }
+    beforeEach(function() {
+      this.currentTest.cell = {
+        columnId: testDescription,
+        rowId: this.currentTest.title
+      };
+    });
     let credentials;
     before(async function() {
       credentials = await generateTestData({...testDataOptions, optionalTests});
     });
     it('If the "proof" field is missing, an error MUST be raised.',
       async function() {
-        this.test.cell = {columnId, rowId: this.test.title};
         const credential = credentials.clone('issuedVc');
         delete credential.proof;
         await verificationFail({credential, verifier});
       });
     it('If the "proof" field is invalid, an error MUST be raised.',
       async function() {
-        this.test.cell = {columnId, rowId: this.test.title};
         const credential = credentials.clone('issuedVc');
         credential.proof = null;
         await verificationFail({credential, verifier});
       });
     it('If the "proof.type" field is missing, an error MUST be raised.',
       async function() {
-        this.test.cell = {columnId, rowId: this.test.title};
         const credential = credentials.clone('issuedVc');
         delete credential.proof.type;
         await verificationFail({credential, verifier});
@@ -46,38 +48,32 @@ export function runDataIntegrityProofVerifyTests({
     it(`If the "proof.type" field is not the string ` +
       `"${expectedProofType}", an error MUST be raised.`,
     async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       const credential = credentials.clone('invalidProofType');
       await verificationFail({credential, verifier});
     });
     it('If the "proof.verificationMethod" field is missing, an error ' +
       'MUST be raised.', async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       const credential = credentials.clone('noVm');
       await verificationFail({credential, verifier});
     });
     it('If the "proof.verificationMethod" field is invalid, an error ' +
       'MUST be raised.', async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       const credential = credentials.clone('invalidVm');
       await verificationFail({credential, verifier});
     });
     it('If the "proof.proofPurpose" field is missing, an error MUST ' +
       'be raised.', async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       const credential = credentials.clone('noProofPurpose');
       await verificationFail({credential, verifier});
     });
     it('If the "proof.proofPurpose" field is invalid, an error MUST ' +
       'be raised.', async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       const credential = credentials.clone('invalidProofPurpose');
       await verificationFail({credential, verifier});
     });
     it('If the "proof.proofPurpose" value does not match ' +
       '"options.expectedProofPurpose", an error MUST be raised.',
     async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       const credential = credentials.clone('issuedVc');
       await verificationFail({
         credential, verifier, options: {
@@ -89,7 +85,6 @@ export function runDataIntegrityProofVerifyTests({
     });
     it('If the "proof.proofValue" field is missing, an error MUST ' +
       'be raised.', async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       // proofValue is added after signing so we can
       // safely delete it for this test
       const credential = credentials.clone('issuedVc');
@@ -98,7 +93,6 @@ export function runDataIntegrityProofVerifyTests({
     });
     it('If the "proof.proofValue" field is invalid, an error MUST be ' +
       'raised.', async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       // null should be an invalid proofValue for almost any proof
       const credential = credentials.clone('issuedVc');
       credential.proof.proofValue = null;
@@ -107,26 +101,21 @@ export function runDataIntegrityProofVerifyTests({
     if(optionalTests?.created) {
       it('If the "proof.created" field is invalid, an error MUST be ' +
         'raised.', async function() {
-        this.test.cell = {columnId, rowId: this.test.title};
         const credential = credentials.clone('invalidCreated');
         await verificationFail({credential, verifier});
       });
     }
     it('If the "proof.proofValue" field is not multibase-encoded, an error ' +
       'MUST be raised.', async function() {
-      this.test.cell = {columnId, rowId: this.test.title};
       const credential = credentials.clone('issuedVc');
-
       // Remove the multibase header to cause validation error
       credential.proof.proofValue = credential.proof.proofValue.slice(1);
-
       await verificationFail({credential, verifier});
     });
     if(optionalTests.authentication) {
       it('If the "options.domain" is set and it does not match ' +
         '"proof.domain", an error MUST be raised.',
       async function() {
-        this.test.cell = {columnId, rowId: this.test.title};
         const credential = credentials.clone('invalidDomain');
         await verificationFail({
           credential, verifier, options: {
@@ -136,7 +125,6 @@ export function runDataIntegrityProofVerifyTests({
       });
       it('If the "options.challenge" is set and it does not match ' +
         '"proof.challenge", an error MUST be raised.', async function() {
-        this.test.cell = {columnId, rowId: this.test.title};
         const credential = credentials.clone('invalidChallenge');
         await verificationFail({
           credential, verifier, options: {
