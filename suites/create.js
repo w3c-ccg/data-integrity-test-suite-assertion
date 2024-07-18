cf67d480cec51c0b8d9f5af7808bbb2750f46d19
Patrick <patrick.st-louis@opsecid.ca>
2024-07-18 15:13:46 +0000

    enable last skipped test
    Signed-off-by: Patrick <patrick.st-louis@opsecid.ca>

--- suites/create.js
+++ suites/create.js
@@ -349,7 +349,7 @@ export function runDataIntegrityProofFormatTests({
               }
             }
           });
-          it.skip('If any element of previousProof array has an id attribute ' +
+          it('If any element of previousProof array has an id attribute ' +
           'that does not match the id attribute of any element of allProofs, ' +
           'an error MUST be raised and SHOULD convey an error type of ' +
           'PROOF_GENERATION_ERROR.',
