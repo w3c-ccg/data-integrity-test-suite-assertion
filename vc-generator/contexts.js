a12b60a3c08ee4414c973d21ed6c990d1f5e2117
Andrew Jones <ajones@digitalbazaar.com>
2024-08-02 20:49:29 +0000

    DRY term addition to copyTerm helper.

--- vc-generator/contexts.js
+++ vc-generator/contexts.js
@@ -2,7 +2,8 @@
  * Copyright (c) 2023-2024 Digital Bazaar, Inc.
  */
 import {
-  contexts as credentialsContexts
+  contexts as credentialsContexts,
+  named as namedCredentialsContexts
 } from '@digitalbazaar/credentials-context';
 import dataIntegrityCtx from '@digitalbazaar/data-integrity-context';
 import didCtx from '@digitalcredentials/did-context';
@@ -10,11 +11,21 @@ import multikeyCtx from '@digitalbazaar/multikey-context';
 
 const contextMap = new Map(credentialsContexts);
 
+const {context: v2Context} = namedCredentialsContexts.get('v2');
+copyTerm({
+  context: v2Context,
+  oldTerm: 'DataIntegrityProof',
+  newTerm: 'UnknownProofType'
+});
 const _dataIntegrityCtx = structuredClone(dataIntegrityCtx.CONTEXT);
+copyTerm({
+  context: _dataIntegrityCtx,
+  oldTerm: 'DataIntegrityProof',
+  newTerm: 'UnknownProofType'
+});
 const diCtx = _dataIntegrityCtx['@context'];
 // add UnknownProofType to local context for test data
-diCtx.UnknownProofType =
-  structuredClone(_dataIntegrityCtx['@context'].DataIntegrityProof);
+diCtx.UnknownProofType = structuredClone(diCtx.DataIntegrityProof);
 // add invalidPurpose to context for test data
 diCtx.DataIntegrityProof['@context'].proofPurpose['@context'].invalidPurpose = {
   '@id': 'https://w3id.org/security#invalidPurpose',
@@ -33,5 +44,10 @@ contextMap.set(
   didCtx.contexts.get(
     didCtx.constants.DID_CONTEXT_URL)
 );
+
+function copyTerm({context, oldTerm, newTerm}) {
+  const ctx = context['@context'];
+  ctx[newTerm] = structuredClone(ctx[oldTerm]);
+}
 
 export {contextMap};
