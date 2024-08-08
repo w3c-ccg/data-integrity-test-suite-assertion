9ab0bf01547c110d799705bbc2fb33c220f3311a
Andrew Jones <ajones@digitalbazaar.com>
2024-08-08 16:57:47 -0400

    First attempt: run all VC types on issuer tests.

--- tests/10-issuer.js
+++ tests/10-issuer.js
@@ -7,6 +7,7 @@ import {cryptosuites} from './fixtures/constants.js';
 import {documentLoader} from '../vc-generator/documentLoader.js';
 import {getMultiKey} from './fixtures/keys/index.js';
 import {MockIssuer} from './mock-data.js';
+import {versionedCredentials} from './fixtures/credentials/index.js';
 
 const tag = 'Test-Issuer-Valid';
 const tags = [tag];
@@ -18,26 +19,45 @@ describe('Test checkDataIntegrityProofFormat()', function() {
       tag: 'Test-Issuer'
     });
   });
+});
+
+describe('should issue all suites', function() {
   for(const [suiteName, testDataOptions] of cryptosuites) {
-    describe('should run issuer tests with suite ', async function() {
-      const implemented = new Map();
-      before(async function() {
+    for(const [vcVersion, credential] of versionedCredentials) {
+      _runSuite({
+        suiteName,
+        vcVersion,
+        testDataOptions,
+        credential
+      });
+    }
+  }
+});
+
+function _runSuite({suiteName, vcVersion, testDataOptions, credential}) {
+  return describe(`VC ${vcVersion} Suite ${suiteName}`, async function() {
+    const implemented = new Map();
+    before(async function() {
+      try {
         const key = await getMultiKey({
           ...testDataOptions
         });
         const {mandatoryPointers, cryptosuite} = testDataOptions;
         const signer = key.signer();
         const suite = createSuite({signer, cryptosuite, mandatoryPointers});
-        const issuer = new MockIssuer({tags, suite, documentLoader});
+        // pass the VC's context to the issuer
+        const {'@context': contexts} = credential;
+        const issuer = new MockIssuer({tags, suite, contexts, documentLoader});
         implemented.set(suiteName, {endpoints: [issuer]});
-      });
-      it(suiteName, function() {
-        checkDataIntegrityProofFormat({
-          implemented,
-          tag,
-          cryptosuiteName: suiteName
-        });
-      });
+      } catch(e) {
+        console.error(e);
+      }
     });
-  }
-});
+    checkDataIntegrityProofFormat({
+      implemented,
+      tag,
+      credential,
+      cryptosuiteName: suiteName
+    });
+  });
+}
