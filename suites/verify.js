af3bfe48d759e2a34fe5dda5c106b8c8505d4725
Andrew Jones <ajones@digitalbazaar.com>
2024-08-15 07:41:14 -0400

    Refactor expectedProofPurpose test.

--- suites/verify.js
+++ suites/verify.js
@@ -89,13 +89,11 @@ export function runDataIntegrityProofVerifyTests({
         'proof.proofPurpose, an error MUST be raised and SHOULD convey an ' +
         'error type of PROOF_VERIFICATION_ERROR.', async function() {
       this.test.link = 'https://w3c.github.io/vc-data-integrity/#:~:text=If%20expectedProofPurpose%20was%20given%2C%20and%20it%20does%20not%20match%20proof.proofPurpose%2C%20an%20error%20MUST%20be%20raised%20and%20SHOULD%20convey%20an%20error%20type%20of%20PROOF_VERIFICATION_ERROR.';
-      const credential = credentials.clone('invalidProofPurpose');
+      // NOTE: expectedProofPurpose should be specified by verifiers
       await verificationFail({
-        credential, verifier, options: {
-          // this will fail since the vc generated is created with the
-          // assertionMethod proof purpose.
-          expectedProofPurpose: 'authentication'
-        }
+        credential: credentials.clone('invalidProofPurpose'),
+        verifier,
+        reason: 'Verifier should reject VC with invalid proof purpose.'
       });
     });
     it('The proofValue property MUST be used, as specified in 2.1 Proofs.',
