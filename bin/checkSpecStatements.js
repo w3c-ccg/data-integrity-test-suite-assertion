528c59778faa26bfd9b531f38af9bb4cbf1319c3
Andrew Jones <ajones@digitalbazaar.com>
2024-09-09 16:21:14 -0400

    Add yargs and start binary cli.

---
+++ bin/checkSpecStatements.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import {argv} from 'node:process';
+import {writeFile} from 'node:fs/promises';
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+
+yargs(hideBin(argv))
+  .scriptName('specStatements')
+  .usage('Usage: $0 <command>')
+  .command(
+    'check',
+    'Checks the spec\'s normative statements again the specs specified',
+    {
+      handler: checkSpecHandler,
+      builder: _yargs => _yargs
+        .option('specUrl', {
+          type: 'string',
+          describe: 'The URL of a specification',
+          demandOption: true
+        })
+    }
+  ).demandCommand().argv;
+
+async function checkSpecHandler(commands) {
+
+}
