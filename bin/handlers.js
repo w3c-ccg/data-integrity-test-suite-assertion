ea463693a8ff5d38a7fe12504ac33685f10590a9
Andrew Jones <ajones@digitalbazaar.com>
2024-09-10 11:36:36 -0400

    Add parseSpec logic & start parsing specs.

--- bin/handlers.js
+++ bin/handlers.js
@@ -2,7 +2,30 @@
  * Copyright (c) 2024 Digital Bazaar, Inc.
  */
 import {writeFile} from 'node:fs/promises';
+import {parse, TextNode, HTMLElement} from 'node-html-parser';
 
 export async function checkSpecText({specUrl, suiteLog}) {
   const specUrls = Array.isArray(specUrl) ? specUrl : [specUrl];
+  for(const url of specUrls) {
+    parseSpec(url);
+  }
+}
+
+function visit(nodes) {
+  for(const node of nodes) {
+    if(node instanceof TextNode) {
+      const {text} = node;
+      if(text.includes('MUST')) {
+        console.log(text);
+      }
+    }
+    visit(node.childNodes);
+  }
+}
+
+async function parseSpec(url) {
+  const response = await fetch(url);
+  const html = await response.text();
+  const spec = parse(html);
+  visit(spec.childNodes);
 }
