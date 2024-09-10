968bd4dd385440699a7aab69096b1ade6c2487d1
Andrew Jones <ajones@digitalbazaar.com>
2024-09-10 13:43:35 -0400

    Abstract into a Visitor class with condition and accumulator.

--- bin/handlers.js
+++ bin/handlers.js
@@ -2,30 +2,48 @@
  * Copyright (c) 2024 Digital Bazaar, Inc.
  */
 import {parse, TextNode} from 'node-html-parser';
-import {writeFile} from 'node:fs/promises';
 
-export async function checkSpecText({specUrl, suiteLog}) {
-  const specUrls = Array.isArray(specUrl) ? specUrl : [specUrl];
-  for(const url of specUrls) {
-    parseSpec(url);
+class Visitor {
+  constructor({condition, accumulator}) {
+    this.condition = condition;
+    this.accumulator = accumulator;
   }
-}
-
-function visit(nodes) {
-  for(const node of nodes) {
-    if(node instanceof TextNode) {
-      const {text} = node;
-      if(text.includes('MUST')) {
-        console.log(text);
+  visit({nodes}) {
+    for(const node of nodes) {
+      if(this.condition(node)) {
+        this.accumulator.push(node);
       }
+      this.visit({nodes: node.childNodes});
     }
-    visit(node.childNodes);
   }
 }
 
-async function parseSpec(url) {
+export async function checkSpecText({specUrl}) {
+  const specUrls = Array.isArray(specUrl) ? specUrl : [specUrl];
+  const accumulator = [];
+  const visitor = new Visitor({
+    condition: textNodeWithMust,
+    accumulator
+  });
+  for(const url of specUrls) {
+    await parseSpec({url, visitor});
+  }
+  console.log(accumulator);
+}
+
+function textNodeWithMust(node) {
+  if(node instanceof TextNode) {
+    const {text} = node;
+    if(text.includes('MUST')) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function parseSpec({url, visitor}) {
   const response = await fetch(url);
   const html = await response.text();
   const spec = parse(html);
-  visit(spec.childNodes);
+  return visitor.visit({nodes: spec.childNodes});
 }
