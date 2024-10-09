8e6dc372a353185c433f2730614deacd2a07bd19
Andrew Jones <ajones@digitalbazaar.com>
2024-10-09 14:55:51 +0000

    Use require's MODULE_NOT_FOUND to return null.

--- vc-generator/staticFixtures.js
+++ vc-generator/staticFixtures.js
@@ -1,7 +1,6 @@
 /*!
  * Copyright 2024 Digital Bazaar, Inc.
  */
-import {access, constants} from 'node:fs/promises';
 import {createRequire} from 'node:module';
 
 const require = createRequire(import.meta.url);
@@ -59,25 +58,12 @@ export async function getStaticFile({
   const filePath = `../inputs/${suiteName}/${version}-${fileName}` +
     `-SimpleSigned2.json`;
   try {
-    const canRead = await canAccess({
-      path: filePath,
-      permission: constants.R_OK
-    });
-    if(!canRead) {
-      return null;
-    }
-    console.log({canRead, filePath});
     return require(filePath);
   } catch(e) {
-    return null;
-  }
-}
-
-async function canAccess({path, permission = constants.R_OK}) {
-  try {
-    await access(path, permission);
-    return true;
-  } catch(e) {
-    return false;
+    // if the module is not found return null
+    if(e?.code === 'MODULE_NOT_FOUND') {
+      return null;
+    }
+    throw e;
   }
 }
